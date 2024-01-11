2755e3f51166193e60c926449cf55e8d46d47292
sri-786 <ganjanaboinasrikanth@gmail.com>
2024-01-11 09:38:11 +0530

    second commit

--- src/testng_practice/TestNG.java
+++ src/testng_practice/TestNG.java
@@ -7,8 +7,13 @@ public class TestNG {
   public void f() {
 	  System.out.println("new testng");
   }
-	 @Test
+
+  @Test
   public void f1() {
 	  System.out.println("new testng add");
+  }
+  @Test
+  public void f2() {
+	  System.out.println("Welcome to TestNG");
   }
 }
